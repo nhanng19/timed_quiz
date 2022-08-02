c5b3d0225d0ab886f36189ffd6d27149d25ba4a3
nhanng19 <nhanng19@gmail.com>
2022-08-02 15:41:32 -0700

    main script file committed

---
+++ assets/js/script.js
@@ -0,0 +1,158 @@
+// List of Questions and Answers
+
+var questions = [
+    {
+        prompt: "Inside which HTML element do we put the JavaScript?",
+        options: ["<javascript>", "<js>", "<script>", "<scripting>"],
+        answer: "<script>"
+    },
+
+    {
+        prompt: "How do you call a function named myFunction?",
+        options: ["call myFunction()", "myFunction()", "call function myFunction", "Call.myFunction"],
+        answer: "myfunction()"
+    },
+
+    {
+        prompt: "How does a for loop start?",
+        options: ["for (i = 0; i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5", " for (i <= 5; i++)"],
+        answer: "for (i = 0; i <= 5; i++)"
+    },
+
+    {
+        prompt: "In JavaScript, which of the following is a logical operator?",
+        options: ["|", "&&", "%", "/"],
+        answer: "&&" 
+    },
+
+    {
+        prompt: "A named element in a JavaScript program that is used to store and retrieve data is a _____.",
+        options: ["method", "assignment operator", "variable", "string"],
+        answer: "variable"
+    }];
+
+// Get Dom Elements
+
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#timer");
+var choicesEl = document.querySelector("#options");
+var submitBtn = document.querySelector("#submit-score");
+var startBtn = document.querySelector("#start");
+var nameEl = document.querySelector("#name");
+var feedbackEl = document.querySelector("#feedback");
+var reStartBtn = document.querySelector("#restart");
+
+// Quiz's initial state
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+// Start quiz and hide frontpage
+
+function quizStart() {
+    timerId = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    var landingScreenEl = document.getElementById("start-screen");
+    landingScreenEl.setAttribute("class", "hide");
+    questionsEl.removeAttribute("class");
+    getQuestion();
+}
+
+// Loop through array of questions and answers and create list with buttons
+
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+  var promptEl = document.getElementById("question-words")
+    promptEl.textContent = currentQuestion.prompt;
+    choicesEl.innerHTML = "";
+    currentQuestion.options.forEach(function(choice, i) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.textContent = i + 1 + ". " + choice;
+        choiceBtn.onclick = questionClick;
+        choicesEl.appendChild(choiceBtn);
+    });
+}
+
+// Check for right answers and deduct time for wrong answer, go to next question
+
+function questionClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      time -= 10;
+      if (time < 0) {
+        time = 0;
+      }
+      timerEl.textContent = time;
+      feedbackEl.textContent = `Wrong! The correct answer was ${questions[currentQuestionIndex].answer}.`;
+      feedbackEl.style.color = "red";
+    } else {
+      feedbackEl.textContent = "Correct!";
+      feedbackEl.style.color = "green";
+    }
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 2000);
+    currentQuestionIndex++;
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+}
+
+// End quiz by hiding questions, stop timer and show final score
+
+function quizEnd() {
+    clearInterval(timerId);
+    var endScreenEl = document.getElementById("quiz-end");
+    endScreenEl.removeAttribute("class");
+    var finalScoreEl = document.getElementById("score-final");
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute("class", "hide");
+}
+
+// End quiz if timer reaches 0
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+    if (time <= 0) {
+      quizEnd();
+    }
+}
+
+// Save score in local storage along with users' name
+
+function saveHighscore() {
+    var name = nameEl.value.trim();
+    if (name !== "") {
+      var highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];
+      var newScore = {
+        score: time,
+        name: name
+      };
+      highscores.push(newScore);
+      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    }
+}
+
+// Save users' score after pressing enter
+
+function checkForEnter(event) {
+    if (event.key === "Enter") {
+        saveHighscore();
+    }
+}
+nameEl.onkeyup = checkForEnter;
+
+// Save users' score after clicking submit
+
+submitBtn.onclick = saveHighscore;
+
+// Start quiz after clicking start quiz
+
+startBtn.onclick = quizStart;
+
